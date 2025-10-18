10e7afd2e6bab3653b3b0f8b53b8475cdd77fa30
Aisha S Campos <aisha.campos@g.austincc.edu>
2025-10-18 02:40:45 +0000

    function for user input

--- src/question_2/main.cpp
+++ src/question_2/main.cpp
@@ -1,6 +1,17 @@
+#include <iostream>
+#include "question2.h"
+using namespace std;
 
 int main()
 {
+    double mass, velocity;
+    cout << "enter mass (kg): ";
+    cin >> mass;
+    cout << "enter velocity (m/s):";
+    cin >> velocity;
+
+    double kinetic_energy = get_kinetic_energy(mass, velocity);
+    cout<<"Kinetic Energy:" << kinetic_energy << "Joules"<< endl;
 
     return 0;
 }
