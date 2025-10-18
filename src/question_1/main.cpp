70216f4f43e801b7ab15b244f00eb3e83a9fe5e3
Aisha S Campos <aisha.campos@g.austincc.edu>
2025-10-18 02:18:00 +0000

    sales commission with user input

--- src/question_1/main.cpp
+++ src/question_1/main.cpp
@@ -8,6 +8,8 @@ int main()
     cout << "Enter sales amount: ";
     double sales;
     cin >> sales;
+    double commission = get_sales_commission(sales);
+    cout << "Sales Commission: $" << commission << endl;
 
     return 0;
 }
