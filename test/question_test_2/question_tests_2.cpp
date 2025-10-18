7dcba258fb7f229c391c57db5b6552b24d5989e4
Aisha S Campos <aisha.campos@g.austincc.edu>
2025-10-18 02:32:21 +0000

    test case for kinetic energy

--- test/question_test_2/question_tests_2.cpp
+++ test/question_test_2/question_tests_2.cpp
@@ -10,3 +10,21 @@ TEST_CASE("test")
 {
 	REQUIRE(test_config() == true);
 }
+TEST_CASE("get kinetic energy")
+{
+	// mass = 2 kg, velocity = 3 m/s -> KE = 0.5 * m * v^2 = 9 J
+	REQUIRE(get_kinetic_energy(2.0, 3.0) == Approx(9.0));
+
+	// zero mass or velocity yields zero energy
+	REQUIRE(get_kinetic_energy(0.0, 5.0) == Approx(0.0));
+	REQUIRE(get_kinetic_energy(4.0, 0.0) == Approx(0.0));
+}
+TEST_CASE("get potential energy")
+{
+	// mass = 2 kg, height = 5 m, g = 9.81 m/s^2 -> PE = m * g * h = 98.1 J
+	REQUIRE(get_potential_energy(2.0, 5.0) == Approx(98.1));
+
+	// zero mass or height yields zero energy
+	REQUIRE(get_potential_energy(0.0, 10.0) == Approx(0.0));
+	REQUIRE(get_potential_energy(3.0, 0.0) == Approx(0.0));
+}
