4cc9d1bc63900caaec9867ab363fbf368abf01a7
Aisha S Campos <aisha.campos@g.austincc.edu>
2025-10-18 02:03:54 +0000

    if else statements

--- src/question_1/question1.cpp
+++ src/question_1/question1.cpp
@@ -1,6 +1,19 @@
 #include "question1.h"
+#include get_sales_commission
 
 bool test_config()
+double get_sales_commission(double sales)
 {
+    if (sales < 0){
+        return 0.0;
+    } else if (sales <= 499){
+        return sales * 0.05;
+    } else if (sales <= 999){
+        return sales * 0.06;
+    } else if (sales <= 1499) {
+        return sales * 0.07;
+    } else {
+        return sales * 0.08;
+    }
+    }
     return true;
-}
