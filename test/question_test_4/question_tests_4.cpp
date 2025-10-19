7282c0df3787fa8ae63905b6f3450d41cc883634
Aisha S Campos <aisha.campos@g.austincc.edu>
2025-10-19 02:35:30 +0000

    test case for roll dice

--- test/question_test_4/question_tests_4.cpp
+++ test/question_test_4/question_tests_4.cpp
@@ -10,3 +10,26 @@ TEST_CASE("test")
 {
 	REQUIRE(test_config() == true);
 }
+
+test_case roll_dice_returns_values_in_range
+{
+	for (int i = 0; i < 100; ++i) {
+		int result = roll_dice();
+		REQUIRE(result >= 1);
+		REQUIRE(result <= 6);
+	}
+}
+
+TEST_CASE ("roll_dice returns different values on multiple calls")
+{
+	bool all_same = true;
+	int first_value = roll_dice();
+	for (int i = 0; i < 10; ++i) {
+		int next_value = roll_dice();
+		if (next_value != first_value) {
+			all_same = false;
+			break;
+		}
+	}
+	REQUIRE(all_same == false);
+}
