6fa474b1ee5516a5dada98df6359857703fad922
Aisha S Campos <aisha.campos@g.austincc.edu>
2025-10-19 02:44:38 +0000

    roll dice function

--- src/question_4/main.cpp
+++ src/question_4/main.cpp
@@ -1,6 +1,21 @@
+#include <iostream>
+#include "question4.h"
+using namespace std;
 
 int main()
 {
+    char choice;
+    do {
+        cout << "Roll the dice? (y/n): ";
+        cin >> choice;
+
+        if (choice == 'y' || choice == 'Y') {
+            int result = roll_dice();
+            cout << "You rolled a" << result << "!" << endl;
+        } while (choice == 'y' || choice == 'Y');
+
+        cout << "Goodbye!" << endl;
+    }
 
     return 0;
 }
--- src/question_4/question4.cpp
+++ src/question_4/question4.cpp
@@ -1,5 +1,15 @@
 //
+#include "question4.h"
+#include <random>
 bool test_config()
 {
     return true;
+}
+
+int roll_dice() {
+    static std::random_device rd;  // obtain a random number from hardware
+    static std::mt19937 eng(rd()); // seed the generator
+    static std::uniform_int_distribution<> distr(1, 6); // define the range
+
+    return distr(eng);
 }
